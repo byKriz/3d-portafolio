516b6adef69879f05621ea3bbf9eece29b80fbc4
bykriz <cristianesquivelh@hotmail.com>
2023-04-02 14:46:39 -0400

    creando los componentes del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { About } from "./components/About";
+import { Experience } from "./components/Experience";
+import { Tech } from "./components/Tech";
+import { Works } from "./components/Works";
+import { Contact } from "./components/Contact";
+import { StarCanvas } from "./components/StarCanvas";
+
 function App() {
   return (
-    <div className="App">
-      <div>3D Developer portfolio</div>
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+
+        <div className=" relative z-0">
+          <Contact />
+          <StarCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
